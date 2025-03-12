12f50ccb1834e8726dae91389cbe8df7d2e98954
WayneStaticScientist <vanderlinkinchat@gmail.com>
2025-03-12 18:38:30 +0200

    added toast

---
+++ utils/toast.ts
@@ -0,0 +1,29 @@
+import { Bounce } from "react-toastify";
+import { toast } from "react-toastify";
+
+export function makeToast(message: string, error = false) {
+    if (error) {
+        return toast.error(message, {
+            position: "top-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: false,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+            transition: Bounce,
+        });
+    }
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: false,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+        transition: Bounce,
+    });
+}
